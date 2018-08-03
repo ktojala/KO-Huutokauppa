07dcfe717e1e4e1d0e3c178a92c0aee0f026fc2e
Ojala Kari T <ktojala@lx7-fuxi179.ad.helsinki.fi>
2018-08-03 15:29:09 +0300

    Tuoteryhmät jatko5

--- application/tasks/views.py
+++ application/tasks/views.py
@@ -2,25 +2,16 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
 from application.tasks.models import Task
-from application.tasks.models import Tuoteryhma
 
 
 @app.route("/tasks", methods=["GET"])
 def tasks_index():
     return render_template("tasks/list.html", tasks = Task.query.all())
 
-@app.route("/tasks", methods=["GET"])
-def tuoteryhmat_index():
-    return render_template("tasks/tuoteryhmat_list.html", tuoteryhmat = Tuoteryhma.query.all())
-
 
 @app.route("/tasks/new/")
 def tasks_form():
     return render_template("tasks/new.html")
-
-@app.route("/tasks/new/")
-def tuoteryhmat_form():
-    return render_template("tasks/newtuoteryhma.html")
 
 
 @app.route("/tasks/<task_id>/", methods=["POST"])
@@ -33,16 +24,6 @@ def tasks_set_done(task_id):
     return redirect(url_for("tasks_index"))
 
 
-@app.route("/tasks/<tuoteryhma_id>/", methods=["POST"])
-def tuoteryhma_muokkaa(task_id):
-
-    t = Tuoteryhma.query.get(tuoteryhma_id)
-    t.done = True
-    db.session().commit()
-  
-    return redirect(url_for("tuoteryhmat_index"))
-
-
 @app.route("/tasks/", methods=["POST"])
 def tasks_create():
     t = Task(request.form.get("name"))
@@ -51,15 +32,6 @@ def tasks_create():
     db.session().commit()
   
     return redirect(url_for("tasks_index"))
-
-@app.route("/tasks/", methods=["POST"])
-def tuoteryhma_create():
-    t = Tuoteryhma(request.form.get("name"))
-
-    db.session().add(t)
-    db.session().commit()
-  
-    return redirect(url_for("tuoteryhmat_index"))
 
 
 
