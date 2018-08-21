fae357a8721022dc512b3740198617fa7d035d3f
Ojala Kari T <ktojala@lx7-fuxi179.ad.helsinki.fi>
2018-08-21 21:47:50 +0300

    Tarjoussivulla peruutusmahdollisuus

--- application/tarjous/forms.py
+++ application/tarjous/forms.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import IntegerField, validators
 
 class TarjousForm(FlaskForm):
-    tarjoussumma = IntegerField("Tarjoukseni (tasaluku euroina)", [validators.NumberRange(min=1, message="Tarjouksen oltava suurempi kuin edellinen tarjous")])
+    tarjoussumma = IntegerField("Tarjoukseni (tasaluku euroina)", [validators.NumberRange(min=0, message="Tarjouksen oltava suurempi kuin edellinen tarjous")])
 
     class Meta:
         csrf = False
