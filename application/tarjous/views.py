93c74ced4169cd9631671bdbcbdb45271b722c02
Ojala Kari T <ktojala@lx7-fuxi179.ad.helsinki.fi>
2018-08-21 23:30:27 +0300

    Tarjous tallentuu myös monesta-moneen -tauluun tarjous

--- application/tarjous/views.py
+++ application/tarjous/views.py
@@ -4,6 +4,7 @@ from flask_login import login_required, current_user
 from application import app, db
 
 from application.myytava.models import Myytava
+from application.tarjous.models import Tarjous
 from application.tarjous.forms import TarjousForm
 from application.auth.models import Asiakas
 
@@ -17,11 +18,21 @@ def tarjous_create(myytava_id):
         return render_template("tarjous/uusitarjous.html", form = form, myytava=t)
 
     if form.validate():
-        if form.tarjoussumma.data > t.tarjoushinta:
-            t.tarjoushinta = form.tarjoussumma.data
-            db.session().commit()
-            return render_template("tarjous/tarjousvahvistus.html", myytava = t)
-        else:
-            return render_template("tarjous/tarjous_error.html", myytava = t)
+        if form.tarjoussumma.data != 0:
+            if form.tarjoussumma.data > t.tarjoushinta:
+                t.tarjoushinta = form.tarjoussumma.data
+                db.session().commit()
+                tt = Tarjous(current_user.id,t.id,form.tarjoussumma.data)
+                db.session().add(tt)
+                db.session().commit()
+                return render_template("tarjous/tarjousvahvistus.html", myytava = t)
+            else:
+                return render_template("tarjous/tarjous_error.html", myytava = t)
 
     return render_template("myytava/myytavan_tiedot.html", myytava = t)
+
+#    def __init__(self, asiakasid,myytavaid,summa):
+#        self.account_id = asiakasid
+#        self.myytava_id = myytavaid
+#        self.tarjoussumma = summa
+
